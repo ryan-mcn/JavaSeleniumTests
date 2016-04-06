043d39bbfe532538ccad7d8d3d4cbede20fcddc3
ryan.padwe <ryan.padwe@mcnhealthcare.com>
2016-04-06 14:07:18 -0600

    Added IE Web Driver code and created initial Firefox test code.

---
+++ src/test/java/Firefox/FirefoxBrowserExecution.java
@@ -0,0 +1,24 @@
+package Firefox;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by ryan.padwe on 4/6/2016.
+ */
+public class FirefoxBrowserExecution {
+
+    public static void main(String[] args)
+
+    {
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("ryanp.ellucid.com");
+
+        System.out.println(driver.getTitle());
+
+    }
+
+
+}
--- src/test/java/IE/IEBrowserExecution.java
+++ src/test/java/IE/IEBrowserExecution.java
@@ -11,7 +11,14 @@ public class IEBrowserExecution {
     public static void main(String[] args)
 
     {
+
+        System.setProperty("webdriver.ie.driver", "C:\\SeleniumDrivers\\IEDriverServer.exe");
+
         WebDriver driver=new InternetExplorerDriver();
+
+        driver.get("ryanp.ellucid.com");
+
+        System.out.println(driver.getTitle());
 
 
     }
