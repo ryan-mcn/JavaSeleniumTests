e1fa6801f825e7ebac6e9c85a87f67395bbf9f37
ryan.padwe <ryan.padwe@mcnhealthcare.com>
2016-04-06 12:48:59 -0600

    Adding Dependencies and initial Firefox Login tests

--- src/test/java/Firefox/Login.java
+++ src/test/java/Firefox/Login.java
@@ -1,5 +1,10 @@
 package Firefox;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
  * Created by ryan.padwe on 4/6/2016.
  */
