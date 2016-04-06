a3a9c6e532846793fecc54349f824e0c60c25b14
ryan.padwe <ryan.padwe@mcnhealthcare.com>
2016-04-06 11:59:44 -0600

    Starting Project

---
+++ src/test/java/Firefox/Login.java
@@ -0,0 +1,65 @@
+package Firefox;
+
+/**
+ * Created by ryan.padwe on 4/6/2016.
+ */
+public class Login {
+
+    public static void main (String[] args)
+    {
+//Initial Load
+        WebDriver driver = new FirefoxDriver();
+
+//Unsuccessful Login UN
+
+        driver.get("https://ryanp.ellucid.com");
+
+        WebElement query = driver.findElement(By.name("sname"));
+
+        query.sendKeys("test.user000");
+
+        query = driver.findElement(By.name("spwd"));
+
+        query.sendKeys("Testing0!");
+
+        driver.findElement(By.id("loginButton")).click();
+
+
+
+//Unsuccessful Login PW
+        driver.get("https://ryanp.ellucid.com");
+
+        query = driver.findElement(By.name("sname"));
+
+        query.sendKeys("test.user");
+
+        query = driver.findElement(By.name("spwd"));
+
+        query.sendKeys("Testing");
+
+        driver.findElement(By.id("loginButton")).click();
+
+
+
+//Successful Login Test
+
+        driver.get("https://ryanp.ellucid.com");
+
+        query = driver.findElement(By.name("sname"));
+
+        query.sendKeys("test.user");
+
+        query = driver.findElement(By.name("spwd"));
+
+        query.sendKeys("Testing0!");
+
+        driver.findElement(By.id("loginButton")).click();
+
+        assert (driver.getTitle().equals("Test User: Home Menu Selection"));
+
+
+
+
+    }
+
+}
---
+++ src/test/java/IE/IEBrowserExecution.java
@@ -0,0 +1,20 @@
+package IE;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+/**
+ * Created by ryan.padwe on 4/6/2016.
+ */
+public class IEBrowserExecution {
+
+    public static void main(String[] args)
+
+    {
+        WebDriver driver=new InternetExplorerDriver();
+
+
+    }
+
+
+}
