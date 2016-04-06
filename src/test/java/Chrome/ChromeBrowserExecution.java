fe3fef32dda3888f3d7cd04ddc3fa9d161c7cae9
ryan.padwe <ryan.padwe@mcnhealthcare.com>
2016-04-06 14:23:57 -0600

    Added Chrome code and created initial Chrome test code

---
+++ src/test/java/Chrome/ChromeBrowserExecution.java
@@ -0,0 +1,26 @@
+package Chrome;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by ryan.padwe on 4/6/2016.
+ */
+public class ChromeBrowserExecution {
+
+    public static void main(String[] args)
+
+    {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");
+
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("ryanp.ellucid.com");
+
+        System.out.println(driver.getTitle());
+
+
+    }
+
+}
